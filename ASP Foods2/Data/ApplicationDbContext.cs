58e2740d8ba1ca0c80b85e65155e41027e3f4e5a
JustMargata <66246000+JustMargata@users.noreply.github.com>
2026-02-11 11:50:40 +0200

    first commit

---
+++ ASP Foods2/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_Foods2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Client>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<TypeProduct> TypeProducts { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ ASP Foods2/Data/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace ASP_Foods2.Data
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ClientId { get; set; }
+        public Client Clients { get; set; }
+        public int ProductId { get; set; }
+        public Product Products { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateAdded { get; set; }
+        //Auto add 
+
+    }
+}
